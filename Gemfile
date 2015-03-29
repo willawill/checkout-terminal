15d90d40244424827082349e2dccae826f6d709e
Willa Wang <willa@neo.com>
2015-03-28 21:04:51 -0400

    Updating README and cleaning gemfile.

--- Gemfile
+++ Gemfile
@@ -1,10 +1,4 @@
 source "https://rubygems.org"
 
 ruby "2.2.1"
-
-gem "rake"
-gem "pry"
-group :development do
-  gem "rspec"
-  gem "rubocop"
-end
+gem "rspec"
--- lib/checkout_terminal.rb
+++ lib/checkout_terminal.rb
@@ -1,7 +1,5 @@
 require "ostruct"
 
-require "pry"
-
 def require_helper(path)
   Dir[File.dirname(__FILE__) + "/checkout_terminal#{path}*.rb"].each do |file|
     require file
